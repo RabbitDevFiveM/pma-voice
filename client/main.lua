e3c33a20248096f9951f1dc0ab06f37c163fd078
emadthasit <em.adthasit@gmaill.com>
2021-12-22 17:47:37 +0700

    Wait

--- client/main.lua
+++ client/main.lua
@@ -23,7 +23,7 @@ callData = {}
 Citizen.CreateThread(function ()
     while ESX == nil do
      TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-     Citizen.Wait(1)
+     Citizen.Wait(0)
     end
    
     while ESX.GetPlayerData() == nil do
